32ce2783df1e8ddcbb7df1b1cc96bf284d2c562c
adan abdulle <77082702+adanabdulle@users.noreply.github.com>
2021-03-17 04:47:03 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyATtk7IM0leMsmlmTnC1rQCdoe55MbOtnY",
-  authDomain: "clone-1fb08.firebaseapp.com",
-  projectId: "clone-1fb08",
-  storageBucket: "clone-1fb08.appspot.com",
-  messagingSenderId: "460580723392",
-  appId: "1:460580723392:web:b6858e902fa4fcccd96b25",
-  measurementId: "G-KPNTHQCDH7",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
